49391310a422f4c0aa7431efa559d194efba8e66
Praveen <spraveen593@gmail.com>
2022-01-01 18:37:41 +0530

    Added headers

--- getCaptcha.js
+++ getCaptcha.js
@@ -1,12 +1,34 @@
 const axios = require("axios");
 const fs = require("fs");
-
+const { v4: uuidv4 } = require("uuid");
 const getCaptcha = async () => {
   try {
     const response = await axios({
       url: "https://tathya.uidai.gov.in/unifiedAppAuthService/api/v2/get/captcha",
       method: "POST",
       data: { langCode: "en", captchaLength: "3", captchaType: "2" },
+      headers: {
+        Accept: "application/json, text/plain",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en_IN",
+        appid: "MYAADHAAR",
+        Connection: "keep-alive",
+        "Content-Length": 109,
+        "Content-Type": "application/json",
+        Host: "tathya.uidai.gov.in",
+        Origin: "https://myaadhaar.uidai.gov.in",
+        Referer: "https://myaadhaar.uidai.gov.in/",
+        "sec-ch-ua-mobile": "?1",
+        "sec-ch-ua-platform": "Android",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-site",
+        token:
+          "STD-x*x-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdHVkZW50X2lkIjoiMjc4ODUiLCJjdXJyZW50X3NlbSI6IjUiLCJjdXJyZW50X3llYXIiOiIzIn0.Cv8C3--MG5MchNDYJw44iOOAbXDkH0UAWjxsZ_XblD0",
+        "User-Agent":
+          "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Mobile Safari/537.36",
+        "x-request-id": `MYAADHAAR:${uuidv4()}`,
+      },
     });
     const imageBuffer = Buffer.from(
       response.data.captchaBase64String,
@@ -17,7 +39,7 @@ const getCaptcha = async () => {
     console.log("Captcha Generated.");
     return response.data.captchaTxnId;
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return console.log("Error while generating QR");
   }
 };
--- getOtp.js
+++ getOtp.js
@@ -12,6 +12,28 @@ const getOtp = async (addhar, txnId, captcha) => {
       captchaValue: captcha,
       transactionId: `MYAADHAAR:${transactionId}`,
     },
+    headers: {
+      Accept: "application/json, text/plain",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Accept-Language": "en_IN",
+      appid: "MYAADHAAR",
+      Connection: "keep-alive",
+      "Content-Length": 109,
+      "Content-Type": "application/json",
+      Host: "tathya.uidai.gov.in",
+      Origin: "https://myaadhaar.uidai.gov.in",
+      Referer: "https://myaadhaar.uidai.gov.in/",
+      "sec-ch-ua-mobile": "?1",
+      "sec-ch-ua-platform": "Android",
+      "Sec-Fetch-Dest": "empty",
+      "Sec-Fetch-Mode": "cors",
+      "Sec-Fetch-Site": "same-site",
+      token:
+        "STD-x*x-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdHVkZW50X2lkIjoiMjc4ODUiLCJjdXJyZW50X3NlbSI6IjUiLCJjdXJyZW50X3llYXIiOiIzIn0.Cv8C3--MG5MchNDYJw44iOOAbXDkH0UAWjxsZ_XblD0",
+      "User-Agent":
+        "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Mobile Safari/537.36",
+      "x-request-id": `MYAADHAAR:${uuidv4()}`,
+    },
   });
   console.log(response.data);
   return response.data;
